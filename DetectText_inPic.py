3a1cf2eabd80f1c2e8905f19d55d92edf66d7634
aattyy-102030 <113221481+aattyy-102030@users.noreply.github.com>
2023-10-28 22:54:06 +0900

    Update DetectText_inPic.py

--- DetectText_inPic.py
+++ DetectText_inPic.py
@@ -1,18 +1,27 @@
 import os
 import zipfile
 from datetime import datetime
+import shutil
 import pytesseract
 from PIL import Image
 import pandas as pd
+import glob
+
+# Input for search query
+search_query = input("The string you want to search for: ")
 
 # Step 1: Rename .docx to .zip and move to './Input/zip'
-docx_folder = './Input/docx'
 zip_folder = './Input/zip'
-os.makedirs(zip_folder, exist_ok=True)
+docx_folder = './Input/docx'
 
+# Clean up './Input/zip' folder after copying
+for zfile in glob.glob(f"{zip_folder}/*.zip"):
+    os.remove(zfile)
+
+# Copy and rename Word files to ZIP folder
 for filename in os.listdir(docx_folder):
     if filename.endswith('.docx'):
-        os.rename(os.path.join(docx_folder, filename), os.path.join(zip_folder, filename.replace('.docx', '.zip')))
+        shutil.copy(os.path.join(docx_folder, filename), os.path.join(zip_folder, filename.replace('.docx', '.zip')))
 
 # Step 2: Unzip files to './Output/(Datetime)_DetectText-InPic'
 current_time = datetime.now().strftime('%Y%m%d-%H%M%S')
@@ -38,9 +47,9 @@ for filename in os.listdir(zip_folder):
                     image = Image.open(image_path)
                     extracted_text = pytesseract.image_to_string(image, lang='eng')
 
-                    if 'JAPAN' in extracted_text:
+                    if search_query in extracted_text:
                         table_data.append({
-                            'Detected Text': 'JAPAN',
+                            'Detected Text': search_query,
                             'Word File': word_filename,
                             'Image File': image_filename
                         })
